8ded88b6839878d5875cc447552671284cd66db8
Al Armato <55386215+zyzalfors@users.noreply.github.com>
2025-04-11 00:14:48 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,9 @@
 package GT3SaveEditor;
 
 public class Main {
-    
+
     public static void main(String[] args) {
-        String path = args.length > 0 ? args[0] : null;
-        new Form(path);
+        new Form(args.length > 0 ? args[0] : null);
     }
 
 }
